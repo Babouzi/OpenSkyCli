656d8c57d259170e9dbe6a40a951d94e77ccd21d
Babouzi <eham.abouzi@efrei.net>
2026-05-16 18:43:21 +0200

    ajout de la requête Http vers opensky

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Http;
+
+Console.WriteLine("Démarrage du radar OpenSky...");
+
+//  Initialisation du client HTTP
+using HttpClient client = new HttpClient();
+
+string url = "https://opensky-network.org/api/states/all";
+Console.WriteLine($"Connexion à l'API : {url}");
+
+// Exécution de la requête (Asynchrone)
+HttpResponseMessage response = await client.GetAsync(url);
+
+// Vérification du statut de la réponse
+response.EnsureSuccessStatusCode();
+
+// Lecture du résultat sous forme de texte 
+string jsonBrut = await response.Content.ReadAsStringAsync();
+
+Console.WriteLine($"Succès ! J'ai récupéré un JSON de {jsonBrut.Length} caractères.");
+
